0968b24e36719eae0d7373205c668c110acc86d5
Niclas Berglind <nb@kjam.se>
2016-12-27 22:06:43 +0100

    Add Intel support

--- root/etc/cont-init.d/10_graphics.sh
+++ root/etc/cont-init.d/10_graphics.sh
@@ -2,7 +2,7 @@
 cd /tmp
 
 # Define drivers supported by the container
-DRIVERS_SUPPORTED=("i915" "nvidia")
+DRIVERS_SUPPORTED=("nvidia" "i810" "i815" "i830" "i845" "i855" "i865" "i915" "i945" "i965")
 
 # Check which drivers are installed on the host OS
 declare -a DRIVERS_DETECTED=(`lshw -c video | awk '/configuration: driver/{print $2}' | cut -d '=' -f 2 | tr '\n' ' ' | xargs`)
@@ -37,22 +37,28 @@ fi
 
 # Take appropriate action depending on provided driver
 case $GFX_DRIVER in
-    nvidia )
-        echo "NVIDIA Driver detected on host system"
-        gfx_version=`cat /proc/driver/nvidia/version | head -n 1 | awk '{ print $8 }'`
-        echo "NVIDIA driver version on host system is $gfx_version"
-        if [ ! -f "/usr/share/nvidia/nvidia-application-profiles-${gfx_version}-rc" ]; then
-                echo "Downloading new NVIDIA driver version ${gfx_version}"
-                nvidia_driver_uri=http://us.download.nvidia.com/XFree86/Linux-x86_64/${gfx_version}/NVIDIA-Linux-x86_64-${gfx_version}.run
-                curl -o nvidia-driver.run $nvidia_driver_uri
-                chmod 755 /tmp/nvidia-driver.run
-                echo "Installing driver"
-                /tmp/nvidia-driver.run -a -N --ui=none --no-kernel-module --run-nvidia-xconfig --silent
-                echo "Driver installed"
-        else
-                echo "NVIDIA driver ${gfx_version} already installed"
-        fi
-        ;;
-    ati_placeholder )
-        ;;
+  nvidia )
+    echo "NVIDIA Driver detected on host OS"
+    gfx_version=`cat /proc/driver/nvidia/version | head -n 1 | awk '{ print $8 }'`
+    echo "NVIDIA driver version on host system is $gfx_version"
+    if [ ! -f "/usr/share/nvidia/nvidia-application-profiles-${gfx_version}-rc" ]; then
+      echo "Downloading new NVIDIA driver version ${gfx_version}"
+      nvidia_driver_uri=http://us.download.nvidia.com/XFree86/Linux-x86_64/${gfx_version}/NVIDIA-Linux-x86_64-${gfx_version}.run
+      curl -o nvidia-driver.run $nvidia_driver_uri
+      chmod 755 /tmp/nvidia-driver.run
+      echo "Installing driver"
+      /tmp/nvidia-driver.run -a -N --ui=none --no-kernel-module --run-nvidia-xconfig --silent
+      echo "Driver installed"
+    else
+      echo "NVIDIA driver ${gfx_version} already installed"
+    fi
+    ;;
+    i810|i815|i830|i845|i855|i865|i915|i945|i965 )
+      echo "Intel Driver detected on host OS"
+      echo "Installing driver"
+      apt-get update && \
+      apt-get -y install libgl1-mesa-glx libgl1-mesa-dri && \
+      rm -rf /var/lib/apt/lists/*
+      echo "Driver installed"
+    ;;
 esac
